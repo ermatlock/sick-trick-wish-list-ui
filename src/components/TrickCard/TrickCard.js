8d0b616ae770064372efe2d3d251339369a14ad7
Eric Matlock <ermatlock@gmail.com>
2022-04-06 12:43:30 -0400

    Create TrickCard

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -1,2 +1,16 @@
 import React from "react"
 import "./TrickCard.css"
+
+const TrickCard = ({stance, name, obstacle, tutorial, id}) => {
+  return (
+    <div className="trick-card" id={id}>
+      <h2>{name}</h2>
+      <p>{`Stance: ${stance}`}</p>
+      <p>{`Obstacle: ${obstacle}`}</p>
+      <a href={tutorial}>{`${name} tutorial`}</a>
+
+    </div>
+  )
+}
+
+export default TrickCard
