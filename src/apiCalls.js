b1bfeb02f93aa5dd1e46de705fa6461e8f0190e6
Eric Matlock <ermatlock@gmail.com>
2022-04-06 13:15:50 -0400

    Create post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,4 +2,18 @@ const getTricks = () => {
   return fetch("http://localhost:3001/api/v1/tricks")
   .then((response) => response.json())
 };
-export { getTricks };
+
+const postTrick = ({stance, name, obstacle, tutorial}) => {
+  return fetch("http://localhost:3001/api/v1/tricks", {
+  method: "POST"
+  body: JSON.stringify({
+    stance: stance,
+    name: name,
+    obstacle: obstacle,
+    tutorial: tutorial
+  }),
+  headers: { "Content-Type": "application/json" },
+})
+}
+
+export { getTricks, postTrick };
